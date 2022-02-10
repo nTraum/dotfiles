c7cab02396240c17733d6ef3c49f48c5aefb5f53
Philipp Preß <ntraum@fastmail.com>
2022-02-10 14:22:21 +0100

    gcf fish alias to check out a branch with fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,19 +1,3 @@
-function is_lappen
-    test "$hostname" = "lappen"
-end
-
-function add_ssh_key_if_missing
-    set ssh_fingerprint $argv[1]
-    set ssh_key = $argv[2]
-
-    ssh-add -l | grep --quiet $ssh_fingerprint
-
-    if test ! $status -eq 0
-        echo "SSH key missing"
-        ssh-add $ssh_key
-    end
-end
-
 # Initializes sharship prompt
 if command -v starship > /dev/null
     starship init fish | source
@@ -33,24 +17,19 @@ abbr --add gds git diff --staged
 
 set -xg EDITOR emacsclient
 
-if status --is-interactive
-    if is_bl_macbook
-        add_ssh_key_if_missing aTXHZ7JAnuxsV1mkP4+2Iva2/w9xg4UaOJeJti2wpAY "$HOME/.ssh/id_ed25519"
-        add_ssh_key_if_missing S/5A8aIh8BB6ergjWIKkQXQofMn9T73IglkrIN4smus "$HOME/.ssh/id_rsa"
-    end
-
-    if is_lappen
-        add_ssh_key_if_missing IpoVpiDDh2UeWuXYJih2b0x77NnBfKxXCNieIcKZc8s "$HOME/.ssh/id_ed25519"
-    end
-end
-
+# FZF
 if command -v fzf > /dev/null
     set -xg FZF_DEFAULT_OPTS '--height 40% --border'
     if command -v fd > /dev/null
         set -xg FZF_DEFAULT_COMMAND  'fd --type=file --hidden --no-ignore-vcs'
     end
+
+    function gcf
+        git checkout (git branch --all | sed "s/.* //" | fzf)
+    end
 end
 
+# bat (cat replacement)
 if command -v bat > /dev/null
     alias cat="bat"
 
