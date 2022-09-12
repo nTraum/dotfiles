8399be50c64a423ecb185c545f64d0b36a7697ff
Philipp Preß <ntraum@fastmail.com>
2022-09-12 14:17:03 +0200

    Remove auto-start of tmux at the end of fish shell, makes projectile error with open terminal failed: not a terminal

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -47,8 +47,3 @@ if command -v tmux > /dev/null
         tmux attach || tmux new
     end
 end
-
-# Start / attach to tmux if we are not in a tmux session already
-if not set --query TMUX
-    tm
-end
