ae6ec9183375c8fdcc01f636297e7283f0a1a1ce
Philipp Preß <ntraum@fastmail.com>
2023-06-27 18:39:02 +0200

    Rubocopify clean up jpg script

--- personal_scripts/cleanup_jpg.rb
+++ personal_scripts/cleanup_jpg.rb
@@ -1,33 +1,33 @@
+# frozen_string_literal: true
+
 # Usage: ruby cleanup_jpg.rb PATH
 # Trashes JPG files from PATH if a corresponding RAW file with the same basename cannot be found.
 # Useful if you shoot JPG+RAWs and then sort out RAWs only in your image library (e.g. darktable).
 
 require 'fileutils'
 
-JPG_GLOB = ".{jpg,jpeg,JPG,JPEG}"
-RAW_GLOB = ".{ARW,arw}"
-TRASH_DIR = (File.expand_path("~/.local/share/Trash/")) + "/"
+JPG_GLOB = '.{jpg,jpeg,JPG,JPEG}'
+RAW_GLOB = '.{ARW,arw}'
+TRASH_DIR = "#{File.expand_path('~/.local/share/Trash/')}/".freeze
 
 path = ARGV[0]
 
-abort("path missing as first argument") unless path
+abort('path missing as first argument') unless path
 
 jpgs = Dir.glob(File.join(path, "*#{JPG_GLOB}"))
 
 to_be_deleted = jpgs.select do |jpg|
   basename = File.basename(jpg, File.extname(jpg))
 
-  (Dir.glob(File.join(path, basename + RAW_GLOB))).empty?
+  Dir.glob(File.join(path, basename + RAW_GLOB)).empty?
 end
 
-abort("No JPGs found that were missing RAW files, exiting...") if to_be_deleted.empty?
+abort('No JPGs found that were missing RAW files, exiting...') if to_be_deleted.empty?
 
-puts "The following JPGs do not have corresponding RAW files:"
+puts 'The following JPGs do not have corresponding RAW files:'
 to_be_deleted.each { |jpg| puts jpg }
 
 puts "Trash #{to_be_deleted.count} files? (yes/NO):"
-answer = STDIN.gets.chomp
+answer = $stdin.gets.chomp
 
-if (answer == "yes" || answer == "y")
-  to_be_deleted.each{ |jpg| FileUtils.mv(jpg, TRASH_DIR) }
-end
+to_be_deleted.each { |jpg| FileUtils.mv(jpg, TRASH_DIR) } if %w[yes y].include?(answer)
