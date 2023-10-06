bac8bdcd47e067ef147e9785901c6927430e92bd
Philipp Preß <ntraum@fastmail.com>
2023-10-06 09:47:15 +0200

    Fish: Add abbrs for mix tasks

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,7 +18,10 @@ abbr --add dc docker-compose
 abbr --add g git
 abbr --add gd git diff
 abbr --add gds git diff --staged
+abbr --add mc mix compile
 abbr --add mp mkdir --parents
+abbr --add mt MIX_ENV=test mix test
+abbr --add mts MIX_ENV=test mix test --stale
 
 # Open file in a running emacs session
 alias em="emacsclient --no-wait"
