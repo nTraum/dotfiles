429d8e4947684712c0a35060f6fe9faeccf2cc21
Philipp Preß <ntraum@fastmail.com>
2025-09-10 14:04:52 +0200

    lua_ls

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -325,10 +325,9 @@ local on_attach = function(_, bufnr)
 end
 
 -- nvim-cmp supports additional completion capabilities
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- Expert
+-- Expert LS (Elixir)
 
 require("lspconfig").lexical.setup({
 	cmd = { "/home/ntraum/coding/expert/expert_linux_amd64" },
@@ -353,18 +352,58 @@ require("lspconfig").lexical.setup({
 -- 	cmd = { "/home/ntraum/coding/elixir-ls/v0.29.2/language_server.sh" },
 -- })
 
--- Lua LS
-lspconfig.lua_ls.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
+vim.lsp.config("lua_ls", {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if
+				path ~= vim.fn.stdpath("config")
+				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+			then
+				return
+			end
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most
+				-- likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Tell the language server how to find Lua modules same way as Neovim
+				-- (see `:h lua-module-load`)
+				path = {
+					"lua/?.lua",
+					"lua/?/init.lua",
+				},
 			},
-		},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+				},
+			},
+		})
+	end,
+	settings = {
+		Lua = {},
 	},
 })
+
+vim.lsp.enable("lua_ls")
+
+-- Lua LS
+-- lspconfig.lua_ls.setup({
+-- 	capabilities = capabilities,
+-- 	on_attach = on_attach,
+-- 	settings = {
+-- 		Lua = {
+-- 			diagnostics = {
+-- 				globals = { "vim" },
+-- 			},
+-- 		},
+-- 	},
+-- })
 
 -- pyright LS (Python)
 lspconfig.pyright.setup({
