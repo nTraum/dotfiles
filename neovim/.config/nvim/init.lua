15f99d4e96a041f0eedce897c11c1bc81e4fc769
Philipp Preß <ntraum@fastmail.com>
2026-06-01 22:07:35 +0200

    increase timeout

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -137,7 +137,7 @@ require("lazy").setup({
 		-- Auto format on save
 		"stevearc/conform.nvim",
 		opts = {
-			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			format_on_save = { timeout_ms = 2000, lsp_fallback = true },
 			formatters = {
 				stylua = {
 					command = "/home/ntraum/bin/stylua",
